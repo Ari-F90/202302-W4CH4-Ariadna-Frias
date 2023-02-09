fd1f9f95539356231443e9260077576b11b74fb8
Ariadna Frias <ariadna.812@gmail.com>
2023-02-09 21:43:39 +0100

    Update app test

--- src/core/components/app/app.test.tsx
+++ src/core/components/app/app.test.tsx
@@ -1,14 +1,20 @@
 import { render } from "@testing-library/react";
 import { App } from "./app";
-import { Keyboard } from "../../../features/keyboard/keyboard";
+import { Keyboard } from "../../../features/components/keyboard/keyboard";
+import { Display } from "../../../features/components/display/display";
+import { Actions } from "../../../features/components/actions/actions";
 
 jest.mock("../../../features/keyboard/keyboard");
+jest.mock("../../../features/display/display");
+jest.mock("../../../features/actions/actions");
 
 describe("Given App component", () => {
   describe("When it is render", () => {
-    test("Then it should call Keyboard", () => {
+    test("Then it should call Keyboard, Display & Actions", () => {
       render(<App />);
       expect(Keyboard).toHaveBeenCalled();
+      expect(Display).toHaveBeenCalled();
+      expect(Actions).toHaveBeenCalled();
     });
   });
 });
